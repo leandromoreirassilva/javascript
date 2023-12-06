1199b115cf5d0dbc2119299379e0b7073b8025d0
leandromoreirassilva <38657167+leandromoreirassilva@users.noreply.github.com>
2023-12-06 14:37:53 -0300

    up

--- desafios/d001/scripts/script.js
+++ desafios/d001/scripts/script.js
@@ -8,19 +8,39 @@ function contagem() {
     const elem2 = document.getElementById("para2");
 
     let i = Number(minc.value) // pega o valor e converte em numero 
-    let f = Number(maxc.value) 
-    let p = Number(passo.value) 
+    let f = Number(maxc.value)
+    let p = Number(passo.value)
 
     let resultado = "";
 
-    for (let count = i ; count <= f; count += p) {
-   
-       resultado = resultado + (count) + ' --> ';     
+    for (let count = i; count <= f; count += p) {
+
+        resultado = resultado + (count) + ' --> ';
     }
 
     resultado = resultado + ' xx! ';
 
     elem2.innerText = (`Contando: `);
     elem.innerText = (resultado);
+
+}
+
+function calcTabuada() {
+
+    let mult_tab = Number(document.getElementById('itab').value)
+    let atab = document.getElementById('itabuada')
+
+    atab.innerHTML = "" // faz com que o 'select' inicie vazio... zerando tudo  
+
+    let c = 1 // obrigatorio 
+    while (c <= 10) {
+        let linha = document.createElement('option')
+        linha.text = `${mult_tab} x ${c} = ${mult_tab * c}`
+        atab.appendChild(linha)
+
+        c++ // obrigatorio 
+    }
+
+
 
 }
